e28555764303a6957cf91f18274dae51a22e9485
pocari <caffelattenonsugar@gmail.com>
2020-11-08 14:17:05 +0900

    文字列、カッコもトークナイズできるように

--- pocarisp.rb
+++ pocarisp.rb
@@ -5,7 +5,44 @@ def read_all(f = $stdin)
 end
 
 class Tokenizer
+  class TokenizeError < StandardError
+    def initialize(input, pos, msg)
+      @input = input
+      @pos = pos
+      @msg = msg
+    end
+
+    def message
+      lines = @input.lines
+      count = @pos
+      target_line_index = lines.find_index {|line| count -= line.size; count <= 0}
+
+      # 最後の行に改行がない場合でもエラメッセージ用につける
+      lines[-1] = lines[-1].chomp + "\n"
+      # 最後の5行ぐらいをエラーメッセージに使う
+      error_lines = lines.each.with_index(1).map { |line, no| [no, line] }[0 .. target_line_index].last(5)
+      # 最後の行の中でのカラムの位置を計算
+      error_column = @pos
+      if target_line_index > 0
+        # 前の行までの文字数全体をposから引くと、最後の行でのカラム位置になる
+        error_column -= lines[0 ..target_line_index - 1].map(&:size).sum
+      end
+      max_line_no, _ = error_lines.max_by {|no, line| no}
+      error_lines << [nil, (" " * (error_column - 1)) + "^\n"]
+      error_lines << [nil, (" " * (error_column - 1)) + @msg.to_s + "\n"]
+      line_no_size = max_line_no.to_s.size
+      error_lines.map { |no, line|
+        if no
+          sprintf("%*d: %s", line_no_size + 1, no, line)
+        else
+          sprintf("%s%s", (' ' * (line_no_size + 1 + 3)), line)
+        end
+      }.join
+    end
+  end
+
   def initialize(input)
+    @input = input
     @s = StringScanner.new(input)
   end
 
@@ -14,11 +51,17 @@ class Tokenizer
   end
 
   def tokenize
+    @s.skip(/\s*/)
+    @current_token_pos = @s.pos
     case
-    when @s.match?(/^\d/)
+    when @s.match?(/\d/)
       [:tk_num, tokenize_num]
-    when @s.match?(/\A"/)
+    when @s.match?(/"/)
       [:tk_str, tokenize_str]
+    when @s.scan(/\(/)
+      [:tk_lparen, nil]
+    when @s.scan(/\)/)
+      [:tk_rparen, nil]
     else
       raise "unknown token"
     end
@@ -32,7 +75,8 @@ class Tokenizer
   def tokenize_str
     tk = @s.scan(/"/)
     buf = []
-    while ch = @s.getch
+    loop do
+      ch = @s.getch
       case ch
       when '"'
         return buf.join
@@ -47,10 +91,20 @@ class Tokenizer
         else
           buf << c2
         end
+      when nil
+        raise tokenize_error('unterminated string literal')
       else
         buf << ch
       end
     end
+  end
+
+  def tokenize_error(msg)
+    TokenizeError.new(
+      @input,
+      @s.pos,
+      msg
+    )
   end
 end
 
--- pocarisp_test.rb
+++ pocarisp_test.rb
@@ -17,4 +17,41 @@ class RispTest <  MiniTest::Test
     tk = Tokenizer.new('"this is \ta \n string"')
     assert_equal [:tk_str, "this is \ta \n string"], tk.next
   end
+
+  def test_read_lparen
+    tk = Tokenizer.new('(')
+    assert_equal [:tk_lparen, nil], tk.next
+  end
+
+  def test_read_rparen
+    tk = Tokenizer.new(')')
+    assert_equal [:tk_rparen, nil], tk.next
+  end
+
+  def test_ignore_white_space
+    tk = Tokenizer.new('   12 \t\n  ')
+    assert_equal [:tk_num, 12], tk.next
+  end
+
+  def test_error_unterminated_string
+    tk = Tokenizer.new(<<~'EOS'.chomp)
+    123
+    "hoge"
+    "foooo
+    EOS
+
+    assert_equal [:tk_num, 123], tk.next
+    assert_equal [:tk_str, "hoge"], tk.next
+    e = assert_raises Tokenizer::TokenizeError do
+      p tk.next
+    end
+
+    assert_equal <<EOS.chomp, e.message.chomp
+ 1: 123
+ 2: "hoge"
+ 3: "foooo
+          ^
+          unterminated string literal
+EOS
+  end
 end
